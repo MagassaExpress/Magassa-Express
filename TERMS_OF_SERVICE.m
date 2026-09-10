2e7856b7300d19bc1260723f7d3a6d34770c1a4e
MagassaExpress <janvierabouta@gmail.com>
2026-09-10 22:08:19 +0000

    Create TERMS_OF_SERVICE.m with user guidelines
    Added the Terms of Service document outlining user obligations, prohibitions, and responsibilities.

---
+++ TERMS_OF_SERVICE.m
@@ -0,0 +1,54 @@
+# TERMS OF SERVICE
+
+## Conditions d'Utilisation
+
+### Acceptation
+
+L'utilisation de Magassa Express implique l'acceptation des présentes conditions.
+
+---
+
+## Obligations des Utilisateurs
+
+Les utilisateurs doivent :
+
+- Fournir des informations exactes
+- Respecter les lois en vigueur
+- Utiliser la plateforme de manière honnête
+- Respecter les autres utilisateurs
+
+---
+
+## Interdictions
+
+Il est interdit de :
+
+- Publier de fausses annonces
+- Utiliser de faux documents
+- Usurper l'identité d'une autre personne
+- Harceler d'autres utilisateurs
+- Utiliser des moyens frauduleux
+
+---
+
+## Responsabilité
+
+Magassa Express met en relation acheteurs et vendeurs.
+
+Les utilisateurs restent responsables de leurs transactions.
+
+---
+
+## Suspension
+
+Magassa Express peut :
+
+- Suspendre un compte
+- Supprimer une annonce
+- Bannir définitivement un utilisateur
+
+en cas de violation des règles.
+
+---
+
+Version : 1.0
